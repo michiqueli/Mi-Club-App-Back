65eb7254784161f149f25e6df99d5d5896fdae99
Nicolas Mennichelli <74836386+michiqueli@users.noreply.github.com>
2024-03-11 02:52:27 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableCors({
+    origin: 'https://mi-club-app-front.vercel.app', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
+    credentials: true, 
+  });
   const config = new DocumentBuilder()
     .setTitle('Admin My Club API')
     .setDescription('This is an API for manage a Club')
